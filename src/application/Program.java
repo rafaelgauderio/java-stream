beff170e17a063bfec23c90ba1d4bb8686047610
Rafael de Luca <deluca1712@gmail.com>
2022-11-24 11:04:50 -0300

    Refatored stream

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,27 +12,34 @@ public class Program {
 		Locale.setDefault(Locale.US);
 
 		List<Integer> list = Arrays.asList(3, 5, 9, 6, 8, 15, 16, 42);
-
-		Stream<Integer> stream01 = list.stream().map(func -> func * 15);
+		
+		System.out.println("Print de original stream");
+		Stream<Integer> stream = list.stream();
+		System.out.println(Arrays.toString(stream.toArray()));
+		
+		System.out.println("\nMultiply stream by 20");
+		Stream<Integer> stream01 = list.stream().map(func -> func * 20);
 		System.out.println(Arrays.toString(stream01.toArray()));
 
+		
 		Stream<String> stream02 = Stream.of("Rafael", "Angela", "Marcelo", "Marcus");
-		System.out.println(Arrays.toString(stream02.toArray()));
-
+		System.out.println("\nStream of Strings:  " + Arrays.toString(stream02.toArray()));
+	
+		System.out.println("\nStream using iteration function");
+		// first element, lambda function that generation the other elements, limit(define the limit of elements)
 		Stream<Integer> stream03 = Stream.iterate(0, func -> func + 6);
-		System.out.println(Arrays.toString(stream03.limit(810).toArray()));
+		System.out.println(Arrays.toString(stream03.limit(50).toArray()));
 
-		System.out.println();
-		System.out.println("Arithmetic Progression ratio equal to 3");
-		Stream<Integer> stream04 = Stream.iterate(0, x -> x + 3);
-		System.out.println(Arrays.toString(stream04.limit(200).toArray()));
+		
+		System.out.println("\nArithmetic Progression ratio equal to 3");
+		Stream<Integer> stream04 = Stream.iterate(3, x -> x + 3);
+		System.out.println(Arrays.toString(stream04.limit(50).toArray()));
 
-		System.out.println();
-		System.out.println("Fibonacci Sequence");
-
-		Stream<Long> stream05 = Stream.iterate(new Long[] { 0L, 1L }, x -> new Long[] { x[1], x[0] + x[1] })
+	
+		System.out.println("\nFibonacci Sequence");
+		Stream<Long> streamFibonacci = Stream.iterate(new Long[] { 0L, 1L }, x -> new Long[] { x[1], x[0] + x[1] })
 				.map(x -> x[0]);
-		System.out.println(Arrays.toString(stream05.limit(50).toArray()));
+		System.out.println(Arrays.toString(streamFibonacci.limit(50).toArray()));
 
 	}
 
